618973ab8924f440e248dcfc7321b70864ef13ba
webProger <vovabatsyk@gmail.com>
2020-04-06 22:51:22 +0300

    first commit

---
+++ vue-app3/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <AddTodo  @add-todo="addTodo"/>
+    <hr />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
+export default {
+  name: "App",
+
+  components: {
+    TodoList,
+    AddTodo
+  },
+  data() {
+    return {
+      title: "Todo Application",
+      todos: [
+        { id: 1, title: "Drink Beer", completed: false },
+        { id: 2, title: "Drink Wine", completed: false },
+        { id: 3, title: "Drink Water", completed: false }
+      ]
+    };
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id != id);
+    },
+    addTodo(todo) {
+      this.todos.unshift(todo) 
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-app3/src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <li>
+        <span :class="{done: todo.completed}">
+            <input type="checkbox" @change="todo.completed = !todo.completed">
+            <strong>{{idx+1}}</strong>
+            {{todo.title}}
+        </span>
+        <button class="rm" @click="$emit('remove-todo', todo.id)">&times;</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true 
+        },
+        idx: Number
+    }
+}
+</script>
+
+<style scoped>
+ li {
+     border: 1px solid #ccc;
+     display: flex;
+     justify-content: space-between;
+     padding: .5rem 2rem;
+     margin-bottom: 1rem;
+ }
+ .rm {
+     background-color: red;
+     border-radius: 50%;
+     font-weight: bold;
+     color: #fff;
+ }
+ input {
+     margin-right: 1rem;
+ }
+ .done {
+     text-decoration: line-through;
+ }
+</style>
